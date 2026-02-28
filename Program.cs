be7c7a2301094ae8707c72a9f521c5f03cd2edc4
Fabricio <elfacio7219@gmail.com>
2026-02-28 15:18:12 -0500

    avance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,95 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Cine
+{
+    
+    
+}
+
+public class Cliente
+{
+    
+}
+
+public class Empleado
+{
+    
+}
+
+public class Sala
+{
+    
+}
+
+public class Asiento
+{
+    
+}
+
+public class Pelicula
+{
+    
+}
+public class Genero
+{
+    
+}
+
+public class Funcion
+{
+    
+}
+public class Boleteria
+{
+    
+}
+
+public class Reserva
+{
+    
+}
+
+public class Venta
+{
+    
+}
+
+public class Pago
+{
+    
+}
+
+public class Factura
+{
+    
+}
+
+public class Promocion
+{
+    
+}
+public class Descuento
+{
+    
+}
+public class Tarifa
+{
+    
+}
+public class Confiteria
+{
+    
+}
+public class Producto
+{
+    
+}
+public class Inventario
+{
+    
+}
+public class Membresia
+{
+    
+}
